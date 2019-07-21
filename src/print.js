7dfdddc129b77790ce306065190f3f3db636a155
张寒冰 <zhanghanbing@meituan.com>
2019-07-21 10:56:22 +0800

    feat: init

---
+++ src/print.js
@@ -0,0 +1,6 @@
+export default function printMe(){
+    console.log("get called from print.js");
+    console.error("restart");
+    console.error("---------");
+    console.log('hotting print.js...');
+}
